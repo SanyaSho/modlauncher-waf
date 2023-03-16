b309a85986f537b98cfc49b0a6744f8808370eea
SanyaSho <68691958+SanyaSho@users.noreply.github.com>
2023-03-16 17:56:02 +0300

    vpk checksum

--- build-vpk.sh
+++ build-vpk.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+rm android/assets/extras_dir.vpk
+
 vpk -c vpk/ android/assets/extras_dir.vpk
+
+export VPKSHA=$(sha1sum android/assets/extras_dir.vpk | cut -d ' ' -f 1)
+
+echo "VPK sha1sum =" $VPKSHA
+
+# aftomathicc
+sed -i "s|VPK_CHSM.*=.*\".*\"|VPK_CHSM = \"$VPKSHA\"|" android/src/me/nillerusr/ExtractAssets.java
